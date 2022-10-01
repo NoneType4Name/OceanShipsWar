ad42fb57e31367623e67edac6ddd2b82c315edc4
AlexKim0710 <90710372+NoneType4Name@users.noreply.github.com>
2022-10-01 23:44:35 +0300

    update.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 jQuery(document).ready(function($){
   
-  $('.button#enter').on('click', function(e){
-    if (document.location.search) location.href = 'osw://' + document.location.search;
-    else location.href = 'osw:'
-  });
+    $('.button#enter').on('click', function(e){
+        if (document.location.search) location.href = 'osw:/' + document.location.search;
+        else location.href = 'osw:/';
+        setTimeout(function(){$("h1").text("Если приложение не запустилось, кажется у вас его нет...")}, 3000);});
   
-  $('.button#download').on('click', function(e){
-    e.preventDefault();
-    location.href="https://github.com/NoneType4Name/OceanShipsWar/releases/latest/download/OceanShipsWar.exe"
-  });
-  
-});
+    $('.button#download').on('click', function(e){
+        e.preventDefault();
+        location.href="https://github.com/NoneType4Name/OceanShipsWar/releases/latest/download/OceanShipsWar.exe";});
+
+    $('.button#about').on('click', function(e){
+        location.href= './about';});
+})
