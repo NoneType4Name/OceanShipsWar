713e8697be9e6c4e0a9f5218464c866dc1b64fbd
AlexKim0710 <90710372+NoneType4Name@users.noreply.github.com>
2022-10-08 13:15:04 +0300

    test update.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,26 @@
+function open_with_arg(args){
+    var split_url = document.location.pathname.split('/');
+    var page_name = split_url[split_url.length-2];
+    var app_name = 'osw://'
+    fetch('https://api.github.com/repos/NoneType4Name/OceanShipsWar/releases/latest').then(function(request){
+                    console.log(request);
+                    return request.json()}).then(function(data){
+        if (data.message!="Not Found"){
+            window.location = app_name + page_name + '/' + data.tag_name + args;
+        }
+        else {
+            alert('project is closed');
+        }})
+}
 $(window).on("load", function() {
-           if (document.location.search) {
-            window.location = 'osw:' + document.location.search.slice(1)};
-        });
+    if (document.location.search) {
+        open_with_arg(document.location.search);
+    };
+});
 jQuery(document).ready(function($){
-  
     $('.button#enter').on('click', function(e){
-        var app_vame = 'osw:'
-        $("h1").text("");
-        if (document.location.search) window.location = app_vame + document.location.search.slice(1);
-        else window.location = app_vame;
-        });
+        open_with_arg(document.location.search);
+    });
   
     $('.button#download').on('click', function(e){
         e.preventDefault();
